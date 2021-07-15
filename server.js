8bdcb41740223331068cf5fbe44816ee6a98d4e1
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-07-15 19:13:44 -0400

    updated local host number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 // Create the express app
 const app = express();
